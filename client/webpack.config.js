4ff84a3667c544e45ee90db26c65e002388fdbd5
derricn2 <derricn2@gmail.com>
2023-09-04 21:29:37 -0700

    updated webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,52 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // generate HTML file with injected scripts
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+
+      // generate manifest.json file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'PWA Text Editor',
+        short_name: 'Text Editor',
+        description: 'A text editor Progressive Web Application',
+        start_url: '/',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        icons: [
+          {
+            src: path.resolve('src/icons/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+        ],
+      }),
+
+      // generate a service worker using Workbox
+      new InjectManifest({
+        swRc: './src/service-worker.js',
+      }),
     ],
 
     module: {
       rules: [
-        
+        // configure CSS loaders and Babel
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],            },
+          },
+        },
       ],
     },
   };
