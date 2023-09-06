bbe548a1b15f7ec3d63d9820b4dd9b4470dad812
derricn2 <derricn2@gmail.com>
2023-09-05 22:48:06 -0700

    troubleshoot database.js logic and worked on install.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,34 +18,37 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 // store content in IndexedDB
 export const putDb = async (content) => {
-  const db = await initdb();
-  const tx = db.transaction('jate', 'readwrite');
+  const jateDb = await openDB();
+  const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
 
   // add content to object store
-  const id = await store.add({ content });
+  const request = store.put({ content });
 
   // wait for transaction to complete
-  await tx.done;
-  console.log(`Content with ID ${id} added to IndexedDB`);
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 // retrieve all contetnt from IndexedDB
 export const getDb = async () => {
-  const db = await initdb();
-  const tx = db.transaction('jate', 'readonly');
+  // connection to database and version to use
+  const jateDb = await openDB('jate', 1);
+  // new transaction and specify database and privileges
+  const tx = jateDb.transaction('jate', 'readonly');
+  // open object store
   const store = tx.objectStore('jate');
 
   // retrieve all contetnt from object store
-  const content = await store.getAll();
+  const request = store.getAll();
 
   // wait for the transaction to complete
-  await tx.done;
-  console.log('Retrieved content from IndexedDB', content);
+  const result = await request;
+  console.log('🚀 - data read from database', result);
 
   // return the retrieved content
-  return content;
+  return result.value;
 };
 
 // initialize database when app starts
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,28 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    // prevent default behavior of event (browser's default install prompt)
+    event.preventDefault();
+
+    // make Install button visible to user
+    butInstall.style.visibility = 'visible';
+
+    // set text on Install button
+    butInstall.textContent = 'Install!'
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    // disable Install button to prevent multiple installations
+    butInstall.setAttribute('disabled', true);
+
+    // change text on Install button to indicate that the PWA is installed
+    butInstall.textContent = 'Installed!';
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    // when PWA is installed, log message 
+    console.log('appinstalled', event);
+});
